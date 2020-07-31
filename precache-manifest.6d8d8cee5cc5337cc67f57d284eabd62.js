81733ba196bbd579c43b7c3598bb405925f583ae
Raul Abril Mestas <rauabrmes@gmail.com>
2020-07-30 20:20:15 -0500

    Updates

---
+++ precache-manifest.6d8d8cee5cc5337cc67f57d284eabd62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d997df289bc09c1e2f52b93b660a2d6c",
+    "url": "/my-app-react0/index.html"
+  },
+  {
+    "revision": "712245b16e87f439d2e1",
+    "url": "/my-app-react0/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef5cb4700f04d9819c92",
+    "url": "/my-app-react0/static/js/2.031a4820.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app-react0/static/js/2.031a4820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "712245b16e87f439d2e1",
+    "url": "/my-app-react0/static/js/main.024afbb5.chunk.js"
+  },
+  {
+    "revision": "b53acf3dd952b82d3460",
+    "url": "/my-app-react0/static/js/runtime-main.b26d553f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app-react0/static/media/logo.5d5d9eef.svg"
+  }
+]);
